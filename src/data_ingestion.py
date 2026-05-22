af12be947bbcfeab21fd7fdab2ee849c31b947cf
Monirul <monirul429588@gmail.com>
2026-05-22 15:03:56 +0600

    Added Params

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import logging
+import yaml
 
 log_dir = "logs"
 
@@ -25,6 +26,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 
@@ -72,10 +90,12 @@ def save_data(train_data : pd.DataFrame, test_data : pd.DataFrame, data_path: st
 
 def main():
     try:
+        params = load_params(params_path="params.yaml")
+        test_size = params["data_ingestion"]["test_size"]
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
         df = load_data(data_url=data_path)
         df = preProcess_data(df)
-        train_data, test_data = train_test_split(df, test_size=0.20, random_state=42)
+        train_data, test_data = train_test_split(df, test_size=test_size, random_state=42)
         save_data(train_data, test_data, data_path = "./data")
       
     except Exception as e:
