c8a1db531a4482148f3c20bb2fb653f302afff13
Monirul <monirul429588@gmail.com>
2026-05-22 15:48:43 +0600

    Added dvc live

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -112,7 +112,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -127,7 +127,7 @@ def main():
             live.log_metric('precision', precision_score(y_test, y_test))
             live.log_metric('recall', recall_score(y_test, y_test))
 
-            # live.log_params(params)
+            live.log_params(params)
         
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
